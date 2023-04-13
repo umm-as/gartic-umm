b63f15afb8af306c6f868342311065564a6d9d91
wjlee611 <wjlee611@gmail.com>
2023-04-12 19:17:42 -0700

    Metro ui init

--- GarticUmm/Program.cs
+++ GarticUmm/Program.cs
@@ -16,7 +16,7 @@ namespace GarticUmm
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new GarticUmm());
         }
     }
 }

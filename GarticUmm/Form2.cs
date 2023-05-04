17ad40298f3820278ef69dd8728e47b474d8df04
dsundert <105479454+dsundert@users.noreply.github.com>
2023-05-04 12:12:21 +0900

    Update Form2.cs

--- GarticUmm/Form2.cs
+++ GarticUmm/Form2.cs
@@ -11,6 +11,7 @@ using MetroFramework.Forms;
 using System.Net.Sockets;
 using System.Threading;
 using System.IO;
+using System.Net;
 
 
 namespace GarticUmm
@@ -18,16 +19,20 @@ namespace GarticUmm
     
     public partial class GUGameForm : MetroForm
     {
-        public NetworkStream m_Stream;
-        public StreamReader m_Read;
-        public StreamWriter m_Write;
-        const int PORT = 2002;
-        private Thread m_ThReader;
+        public NetworkStream m_Stream; //네트워크 스트림
+        public StreamReader m_Read; //읽기
+        public StreamWriter m_Write; //쓰기
+        const int PORT = 2002; //포트번호
+        private Thread m_ThReader; //읽기쓰레드
+        private TcpClient client;
+        private TcpListener m_TcpListener;//서버 작동 리스너
+        private Thread m_thServer; //서버 플래그
 
-        public bool m_bConnected = false;
+        public bool m_bConnected = false; //서버 접속 플래그
         public GUGameForm()
         {
             InitializeComponent();
+            IPAddress[] local_ip = Dns.GetHostAddresses(Dns.GetHostName());
         }
     }
 }

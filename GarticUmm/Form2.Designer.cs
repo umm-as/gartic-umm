49e042e79bf3e28eb9b3f3b67af3c90732ad0f53
정회준 <ghlwns10@naver.com>
2023-05-04 11:30:40 +0900

    툴바를 이용해서 펜의 굵기와 색을 변경하는 버튼과 지우개 버튼 생셩
    툴바를 이용해서 펜의 굵기와 색을 변경하는 버튼과 지우개 버튼 생성

--- GarticUmm/Form2.Designer.cs
+++ GarticUmm/Form2.Designer.cs
@@ -31,24 +31,22 @@
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GUGameForm));
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
-            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
-            this.button10 = new System.Windows.Forms.Button();
+            this.toolBar1 = new System.Windows.Forms.ToolBar();
             this.imageList1 = new System.Windows.Forms.ImageList(this.components);
-            this.button9 = new System.Windows.Forms.Button();
-            this.button8 = new System.Windows.Forms.Button();
-            this.button7 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.toolBar2 = new System.Windows.Forms.ToolBar();
+            this.thinbtn = new System.Windows.Forms.ToolBarButton();
+            this.middlebtn = new System.Windows.Forms.ToolBarButton();
+            this.thickbtn = new System.Windows.Forms.ToolBarButton();
+            this.redbtn = new System.Windows.Forms.ToolBarButton();
+            this.yellowbtn = new System.Windows.Forms.ToolBarButton();
+            this.greenbtn = new System.Windows.Forms.ToolBarButton();
+            this.bluebtn = new System.Windows.Forms.ToolBarButton();
+            this.purplebtn = new System.Windows.Forms.ToolBarButton();
+            this.blackbtn = new System.Windows.Forms.ToolBarButton();
+            this.eraserbtn = new System.Windows.Forms.ToolBarButton();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
-            this.splitContainer2.Panel1.SuspendLayout();
-            this.splitContainer2.SuspendLayout();
             this.SuspendLayout();
             // 
             // splitContainer1
@@ -60,51 +58,20 @@
             // 
             // splitContainer1.Panel1
             // 
-            this.splitContainer1.Panel1.Controls.Add(this.splitContainer2);
+            this.splitContainer1.Panel1.Controls.Add(this.toolBar2);
             this.splitContainer1.Size = new System.Drawing.Size(1045, 770);
             this.splitContainer1.SplitterDistance = 737;
             this.splitContainer1.TabIndex = 0;
             // 
-            // splitContainer2
+            // toolBar1
             // 
-            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.splitContainer2.IsSplitterFixed = true;
-            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
-            this.splitContainer2.Name = "splitContainer2";
-            this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
-            // 
-            // splitContainer2.Panel1
-            // 
-            this.splitContainer2.Panel1.Controls.Add(this.button10);
-            this.splitContainer2.Panel1.Controls.Add(this.button9);
-            this.splitContainer2.Panel1.Controls.Add(this.button8);
-            this.splitContainer2.Panel1.Controls.Add(this.button7);
-            this.splitContainer2.Panel1.Controls.Add(this.button6);
-            this.splitContainer2.Panel1.Controls.Add(this.button5);
-            this.splitContainer2.Panel1.Controls.Add(this.button4);
-            this.splitContainer2.Panel1.Controls.Add(this.button3);
-            this.splitContainer2.Panel1.Controls.Add(this.button2);
-            this.splitContainer2.Panel1.Controls.Add(this.button1);
-            this.splitContainer2.Size = new System.Drawing.Size(737, 770);
-            this.splitContainer2.SplitterDistance = 61;
-            this.splitContainer2.TabIndex = 1;
-            // 
-            // button10
-            // 
-            this.button10.AutoSize = true;
-            this.button10.FlatAppearance.BorderColor = System.Drawing.Color.Black;
-            this.button10.FlatAppearance.BorderSize = 2;
-            this.button10.FlatAppearance.CheckedBackColor = System.Drawing.Color.Transparent;
-            this.button10.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
-            this.button10.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
-            this.button10.ImageIndex = 9;
-            this.button10.ImageList = this.imageList1;
-            this.button10.Location = new System.Drawing.Point(668, 4);
-            this.button10.Margin = new System.Windows.Forms.Padding(0);
-            this.button10.Name = "button10";
-            this.button10.Size = new System.Drawing.Size(60, 55);
-            this.button10.TabIndex = 9;
-            this.button10.UseVisualStyleBackColor = true;
+            this.toolBar1.Dock = System.Windows.Forms.DockStyle.None;
+            this.toolBar1.DropDownArrows = true;
+            this.toolBar1.Location = new System.Drawing.Point(3, 19);
+            this.toolBar1.Name = "toolBar1";
+            this.toolBar1.ShowToolTips = true;
+            this.toolBar1.Size = new System.Drawing.Size(737, 42);
+            this.toolBar1.TabIndex = 0;
             // 
             // imageList1
             // 
@@ -122,158 +89,76 @@
             this.imageList1.Images.SetKeyName(8, "btn_black.png");
             this.imageList1.Images.SetKeyName(9, "btn_eraser.png");
             // 
-            // button9
+            // toolBar2
             // 
-            this.button9.AutoSize = true;
-            this.button9.FlatAppearance.BorderColor = System.Drawing.Color.Black;
-            this.button9.FlatAppearance.BorderSize = 2;
-            this.button9.FlatAppearance.CheckedBackColor = System.Drawing.Color.Transparent;
-            this.button9.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
-            this.button9.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
-            this.button9.ImageIndex = 8;
-            this.button9.ImageList = this.imageList1;
-            this.button9.Location = new System.Drawing.Point(599, 4);
-            this.button9.Margin = new System.Windows.Forms.Padding(0);
-            this.button9.Name = "button9";
-            this.button9.Size = new System.Drawing.Size(60, 55);
-            this.button9.TabIndex = 8;
-            this.button9.UseVisualStyleBackColor = true;
+            this.toolBar2.Buttons.AddRange(new System.Windows.Forms.ToolBarButton[] {
+            this.thinbtn,
+            this.middlebtn,
+            this.thickbtn,
+            this.redbtn,
+            this.yellowbtn,
+            this.greenbtn,
+            this.bluebtn,
+            this.purplebtn,
+            this.blackbtn,
+            this.eraserbtn});
+            this.toolBar2.DropDownArrows = true;
+            this.toolBar2.ImageList = this.imageList1;
+            this.toolBar2.Location = new System.Drawing.Point(0, 0);
+            this.toolBar2.Name = "toolBar2";
+            this.toolBar2.ShowToolTips = true;
+            this.toolBar2.Size = new System.Drawing.Size(737, 44);
+            this.toolBar2.TabIndex = 0;
             // 
-            // button8
+            // thinbtn
             // 
-            this.button8.AutoSize = true;
-            this.button8.FlatAppearance.BorderColor = System.Drawing.Color.Black;
-            this.button8.FlatAppearance.BorderSize = 2;
-            this.button8.FlatAppearance.CheckedBackColor = System.Drawing.Color.Transparent;
-            this.button8.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
-            this.button8.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
-            this.button8.ImageIndex = 7;
-            this.button8.ImageList = this.imageList1;
-            this.button8.Location = new System.Drawing.Point(528, 4);
-            this.button8.Margin = new System.Windows.Forms.Padding(0);
-            this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(60, 55);
-            this.button8.TabIndex = 7;
-            this.button8.UseVisualStyleBackColor = true;
+            this.thinbtn.ImageIndex = 0;
+            this.thinbtn.Name = "thinbtn";
             // 
-            // button7
+            // middlebtn
             // 
-            this.button7.AutoSize = true;
-            this.button7.FlatAppearance.BorderColor = System.Drawing.Color.Black;
-            this.button7.FlatAppearance.BorderSize = 3;
-            this.button7.FlatAppearance.CheckedBackColor = System.Drawing.Color.Transparent;
-            this.button7.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
-            this.button7.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
-            this.button7.ImageIndex = 6;
-            this.button7.ImageList = this.imageList1;
-            this.button7.Location = new System.Drawing.Point(454, 4);
-            this.button7.Margin = new System.Windows.Forms.Padding(0);
-            this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(60, 55);
-            this.button7.TabIndex = 6;
-            this.button7.UseVisualStyleBackColor = true;
+            this.middlebtn.ImageIndex = 1;
+            this.middlebtn.Name = "middlebtn";
             // 
-            // button6
+            // thickbtn
             // 
-            this.button6.AutoSize = true;
-            this.button6.FlatAppearance.BorderColor = System.Drawing.Color.Black;
-            this.button6.FlatAppearance.BorderSize = 2;
-            this.button6.FlatAppearance.CheckedBackColor = System.Drawing.Color.Transparent;
-            this.button6.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
-            this.button6.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
-            this.button6.ImageIndex = 5;
-            this.button6.ImageList = this.imageList1;
-            this.button6.Location = new System.Drawing.Point(381, 4);
-            this.button6.Margin = new System.Windows.Forms.Padding(0);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(60, 55);
-            this.button6.TabIndex = 5;
-            this.button6.UseVisualStyleBackColor = true;
+            this.thickbtn.ImageIndex = 2;
+            this.thickbtn.Name = "thickbtn";
             // 
-            // button5
+            // redbtn
             // 
-            this.button5.AutoSize = true;
-            this.button5.FlatAppearance.BorderColor = System.Drawing.Color.Black;
-            this.button5.FlatAppearance.BorderSize = 2;
-            this.button5.FlatAppearance.CheckedBackColor = System.Drawing.Color.Transparent;
-            this.button5.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
-            this.button5.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
-            this.button5.ImageIndex = 4;
-            this.button5.ImageList = this.imageList1;
-            this.button5.Location = new System.Drawing.Point(304, 4);
-            this.button5.Margin = new System.Windows.Forms.Padding(0);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(60, 55);
-            this.button5.TabIndex = 4;
-            this.button5.UseVisualStyleBackColor = true;
+            this.redbtn.ImageIndex = 3;
+            this.redbtn.Name = "redbtn";
             // 
-            // button4
+            // yellowbtn
             // 
-            this.button4.AutoSize = true;
-            this.button4.FlatAppearance.BorderColor = System.Drawing.Color.Black;
-            this.button4.FlatAppearance.BorderSize = 2;
-            this.button4.FlatAppearance.CheckedBackColor = System.Drawing.Color.Transparent;
-            this.button4.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
-            this.button4.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
-            this.button4.ImageIndex = 3;
-            this.button4.ImageList = this.imageList1;
-            this.button4.Location = new System.Drawing.Point(225, 4);
-            this.button4.Margin = new System.Windows.Forms.Padding(0);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(60, 55);
-            this.button4.TabIndex = 3;
-            this.button4.UseVisualStyleBackColor = true;
+            this.yellowbtn.ImageIndex = 4;
+            this.yellowbtn.Name = "yellowbtn";
             // 
-            // button3
+            // greenbtn
             // 
-            this.button3.AutoSize = true;
-            this.button3.FlatAppearance.BorderColor = System.Drawing.Color.Black;
-            this.button3.FlatAppearance.BorderSize = 2;
-            this.button3.FlatAppearance.CheckedBackColor = System.Drawing.Color.Transparent;
-            this.button3.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
-            this.button3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
-            this.button3.ImageIndex = 2;
-            this.button3.ImageList = this.imageList1;
-            this.button3.Location = new System.Drawing.Point(155, 4);
-            this.button3.Margin = new System.Windows.Forms.Padding(0);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(60, 55);
-            this.button3.TabIndex = 2;
-            this.button3.UseVisualStyleBackColor = true;
+            this.greenbtn.ImageIndex = 5;
+            this.greenbtn.Name = "greenbtn";
             // 
-            // button2
+            // bluebtn
             // 
-            this.button2.AutoSize = true;
-            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.Black;
-            this.button2.FlatAppearance.BorderSize = 2;
-            this.button2.FlatAppearance.CheckedBackColor = System.Drawing.Color.Transparent;
-            this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
-            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
-            this.button2.ImageIndex = 1;
-            this.button2.ImageList = this.imageList1;
-            this.button2.Location = new System.Drawing.Point(83, 4);
-            this.button2.Margin = new System.Windows.Forms.Padding(0);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(60, 55);
-            this.button2.TabIndex = 1;
-            this.button2.UseVisualStyleBackColor = true;
+            this.bluebtn.ImageIndex = 6;
+            this.bluebtn.Name = "bluebtn";
             // 
-            // button1
+            // purplebtn
             // 
-            this.button1.AutoSize = true;
-            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Black;
-            this.button1.FlatAppearance.BorderSize = 2;
-            this.button1.FlatAppearance.CheckedBackColor = System.Drawing.Color.Transparent;
-            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
-            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
-            this.button1.ImageIndex = 0;
-            this.button1.ImageList = this.imageList1;
-            this.button1.Location = new System.Drawing.Point(13, 3);
-            this.button1.Margin = new System.Windows.Forms.Padding(0);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(60, 55);
-            this.button1.TabIndex = 0;
-            this.button1.UseVisualStyleBackColor = true;
+            this.purplebtn.ImageIndex = 7;
+            this.purplebtn.Name = "purplebtn";
+            // 
+            // blackbtn
+            // 
+            this.blackbtn.ImageIndex = 8;
+            this.blackbtn.Name = "blackbtn";
+            // 
+            // eraserbtn
+            // 
+            this.eraserbtn.ImageIndex = 9;
+            this.eraserbtn.Name = "eraserbtn";
             // 
             // GUGameForm
             // 
@@ -286,12 +171,9 @@
             this.Resizable = false;
             this.Text = "Gartic Umm";
             this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
             this.splitContainer1.ResumeLayout(false);
-            this.splitContainer2.Panel1.ResumeLayout(false);
-            this.splitContainer2.Panel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
-            this.splitContainer2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -299,17 +181,18 @@
         #endregion
 
         private System.Windows.Forms.SplitContainer splitContainer1;
-        private System.Windows.Forms.SplitContainer splitContainer2;
         private System.Windows.Forms.ImageList imageList1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button9;
-        private System.Windows.Forms.Button button8;
-        private System.Windows.Forms.Button button7;
-        private System.Windows.Forms.Button button6;
-        private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.ToolBar toolBar1;
+        private System.Windows.Forms.ToolBar toolBar2;
+        private System.Windows.Forms.ToolBarButton thinbtn;
+        private System.Windows.Forms.ToolBarButton middlebtn;
+        private System.Windows.Forms.ToolBarButton thickbtn;
+        private System.Windows.Forms.ToolBarButton redbtn;
+        private System.Windows.Forms.ToolBarButton yellowbtn;
+        private System.Windows.Forms.ToolBarButton greenbtn;
+        private System.Windows.Forms.ToolBarButton bluebtn;
+        private System.Windows.Forms.ToolBarButton purplebtn;
+        private System.Windows.Forms.ToolBarButton blackbtn;
+        private System.Windows.Forms.ToolBarButton eraserbtn;
     }
 }
